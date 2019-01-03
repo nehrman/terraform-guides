fe744a7910ad3ea2b2fe0200c8a802c82403511d
nehrman <nicolas.ehrman@gmail.com>
2019-01-03 16:36:34 +0100

    ..

--- infrastructure-as-code/azure-vm/main.tf
+++ infrastructure-as-code/azure-vm/main.tf
@@ -11,7 +11,9 @@ variable "windows_dns_prefix" {
   description = "DNS prefix to add to to public IP address for Windows VM"
 }
 
-variable "az_rg" {}
+variable "az_rg" {
+  descrption : "Resource Group name to use to deploy VMs"
+}
 
 variable "admin_password" {
   description = "admin password for Windows VM"
